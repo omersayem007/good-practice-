e40aad7b810c419362244d52958920196912ec58
omersayem007 <omer_sayem@yahoo.com>
2017-02-02 16:11:39 +0600

    Update quick-sort.js

--- quick-sort.js
+++ quick-sort.js
@@ -2,28 +2,46 @@ function partition(array,start,end)
 {
   
   var pivot =array[end];
-  var pindex=array[start];
+  var pindex=start;
+  var temp;
   
-  for(var i= pindex ;i<end-1 ;i++)
-    {
+  
+  
+  // for(var i=pindex ; i<end-1 ;i++)
+   {
+    
       if(array[i]<=array[pivot])
         {
+          temp=array[i];
+          array[i]=array[pindex];
+          array[pindex]=temp;
           
+          pindex++;
         }
-        
-    }
+      
+ //  }
+  return array;
+       
+   
+  
+  /*temp=array[end];
+  array[end]=array[pindex];
+  array[pindex]=pivot;
+  
+  return array;*/
+  
 }
 
-function quick_sort(array,start,end)
+/*function quick_sort(array,start,end)
 {
   if(start<end)
     {
       var pivot =partition(array,start,end);
-      quicksort(array,start,pivot-1);
-      quicksort(array,pivot+1 ,end);
+      quick_sort(array,start,pivot-1);
+      quick_sort(array,pivot+1 ,end);
       
     }
-  
-}
+  return array;
+}*/
 
-console.log(quick_sort( [2,7,3,5,4],0,4 ))
+console.log(partition( [7,2,1,6,8,5,3,4],0,7 ) );
