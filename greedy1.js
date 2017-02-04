20782c33e650cdb9e530eabfc9602ee67cdcb985
omersayem007 <omer_sayem@yahoo.com>
2017-02-04 15:59:39 +0600

    Create greedy1.js

---
+++ greedy1.js
@@ -0,0 +1,16 @@
+function greedy(start,end)
+{
+ for(var i = start.sort()[0] ; i<=start.length ; i++   )
+   {
+     var j=i+1;
+     
+     
+   }
+
+ )
+  
+ return ;  
+}
+
+
+console.log(greedy([1, 3, 0 ,5, 8, 5],[2, 4, 6 ,7 ,9, 9] ));
