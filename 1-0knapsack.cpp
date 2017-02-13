84653c7aa05a94b12793fbbb25b65d66d821b1bb
omersayem007 <omer_sayem@yahoo.com>
2017-02-13 17:45:07 +0600

    Create 1-0knapsack.cpp

---
+++ 1-0knapsack.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int knapsack(int weight[],int taka[],int n, int limit)
+{
+    int B[n+1][limit+1];
+
+    for(int i=0;i<=limit;i++)
+    {
+        B[0][i]=0;
+    }
+    for(int i=0;i<=n;i++)
+    {
+        B[i][0]=0;
+    }
+
+   for(int i=1 ;i<=limit;i++)
+    {
+         for(int w=1 ;w<=n;w++)
+
+        {
+
+             if(weight[i]<w)
+        {
+                if ( taka[i]+ B[i-1,w-weight[i]] > B[i-1,w] )
+                {
+            
+                 B[i,w] = taka[i]+B[i-1,w-weight[i];
+                }
+
+			else
+				B[i,w] = B[i-1,w];
+        }
+		else B[i,w] =B[i-1,w]  ;
+
+        }
+
+
+
+    }
+
+
+}
