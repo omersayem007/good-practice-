755d316f0db2f955324d8c1912272318a74cb476
omersayem007 <omer_sayem@yahoo.com>
2017-02-23 23:31:54 +0600

    Update fractional_knapsack.cpp

--- fractional_knapsack.cpp
+++ fractional_knapsack.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
 using namespace std;
-int top, status;
+int top=4, status;
 
 
-void swap(int *x, int *y)
+ void swap(double *x, double *y)
 {
     int temp = *x;
     *x = *y;
     *y = temp;
 }
-int pop (int stack[])
+
+int pop (double  stack[])
 {
     int ret;
     if (top == -1)
@@ -24,11 +25,11 @@ int pop (int stack[])
 return ret;
 }
 
-int knapsack(int *weight,int *index,int n,int limit)
+int knapsack(double *weight,double *index,int n,int limit)
 {
-   int top = -1;
-  int x[n];
-  int curr_weight=0;
+
+   double x[n];
+  int curr_weight=0; // current weight in limit
 
   for( int i =0 ; i<n ; i++)
     {
@@ -39,61 +40,66 @@ int knapsack(int *weight,int *index,int n,int limit)
                  if(curr_weight+weight[j]<limit)
             {
               x[i]=1;
-              curr_weight+=weight[j];
+              curr_weight+=weight[j]; // updating current weight
             }
           else
+
           x[i]=(limit-curr_weight)/weight[j];
 
         }
-    curr_weight=limit;*/
+    curr_weight=limit;
 
     cout<<"boooooooom !!!:"<<endl;
     for(int i=0 ;i<n ;i++)
    {
-     cout<<""<<x[i];
+     cout<<""<<x[i]<<endl;
 
    }
 
   return 0  ;
 
 }
-int main()
+
+  int main()
 {
     int n,small,limit;
     cout<<"enter the size of the array"<<endl;
-    cin>>n;
-    int weight[n],taka[n],index[n] , profit[n],x[n];
+    cin>>n;  // size of total element
+    double weight[n],taka[n],index[n] , profit[n],x[n];
 
-    cout<<"Enter weight times :"<<endl;
+    cout<<"Enter weights :"<<endl;
 
-    for(int i=0;i<n;i++)
-    {
-        cin>>weight[i];
-    }
+                    for(int i=0;i<n;i++)
+                        {
+                            cin>>weight[i];
+                        }
+
     cout<<"Enter taka :"<<endl;
-    for (int i=0;i<n;i++)
-    {
-        cin>>taka[i];
 
-    }
+                        for (int i=0;i<n;i++)
+                            {
+                                cin>>taka[i];
+
+                            }
     cout<<"Enter the max weight limit :"<<endl;
     cin>>limit ;
-    for(int i=0;i<n;i++)
-    {
-      profit[i]=(taka[i]/weight[i]);
-    }
 
-    for (int i=0;i<n;i++)
-    {
-        index[i]=i;
+                            for(int i=0;i<n;i++)
+                                {
+                                    profit[i]=(taka[i]/weight[i]);
+                                }
 
-    }
-     cout<<endl;
-  for (int i=0;i<n;i++)
-    {
-        cout<<index[i]<<" ";
+                for (int i=0;i<n;i++)
+                    {
+                        index[i]=i;
 
-    }
+                    }
+    cout<<endl;
+                        for (int i=0;i<n;i++)
+                            {
+                                cout<<index[i]<<" ";
+
+                            }
     cout<<endl;
     for (int i=0;i<n-1;i++)
     {
@@ -105,6 +111,7 @@ int main()
         swap(&index[small],&index[i]);
     }
     cout<<"the sorted array"<<endl;
+
      for (int i=0;i<n;i++)
     {
         cout<<profit[i]<<" ";
@@ -120,10 +127,11 @@ int main()
     cout<<endl;
 
 
-   knapsack(weight,index,n,limit);
+  knapsack( weight, index ,n,limit);
 
 
 
 
     return 0;
 }
+
