3432179c3769be174c6d2251a16fe42a50b27b39
omersayem007 <omer_sayem@yahoo.com>
2017-04-24 23:17:30 +0600

    Create fun_clouser.js

---
+++ js/fun_clouser.js
@@ -0,0 +1,17 @@
+function kamasuttro( pornstar ){
+ var pornstar2 =" Mia Khalifa" ;  
+console.log("oh "+pornstar+" i m done");
+
+function irection(){
+  
+  console.log(" oh no "+pornstar2+" M irect again !!! ");
+}
+
+setInterval(irection,1000);
+  
+  
+}
+
+
+
+kamasuttro( "mia markova" );
