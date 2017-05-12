6e985fd9273dcda56c82c8176b94f89586ea7e54
omersayem007 <omer_sayem@yahoo.com>
2017-05-13 00:54:38 +0600

    Update destructing.js

--- js/ES6/destructing.js
+++ js/ES6/destructing.js
@@ -2,6 +2,6 @@ function hello(){
   
   return  "hello i m sayem".split(" ") ;
 }
-var [ a, b, c, name , id="you have to find id" ]= hello() ;
+var [ a, b, c, name , id="you have to find id" ]= hello() || [];
 
 console.log( id  );
